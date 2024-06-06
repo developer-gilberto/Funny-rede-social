0c797dc6943999861dd8dc22e43f07a6de345d9b
developer-gilberto <developer.gilberto@gmail.com>
2024-06-05 21:47:18 -0300

    instalei e configurei handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import '../config.js';
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
 const PORT = process.env.PORT || process.env.PORT_DEFAULT;
 
+
+app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('pages/home');
 });
 
 app.listen(PORT, () => console.log(`> Server running http://localhost:${PORT}`));
