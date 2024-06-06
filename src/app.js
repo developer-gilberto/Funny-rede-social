0eeac0d1649cbd9b1ae3a331057d4cd5dd8cb410
developer-gilberto <developer.gilberto@gmail.com>
2024-06-05 21:31:51 -0300

    instalei e configurei dotenv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
+import '../config.js';
 import express from 'express';
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || process.env.PORT_DEFAULT;
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(PORT, () => console.log(`> Server running http://localhost:${PORT})`));
+app.listen(PORT, () => console.log(`> Server running http://localhost:${PORT}`));
