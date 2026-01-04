79839ffc3fca995daad30ff0748e6afbfa589e21
gilberto <developer.gilberto@gmail.com>
2026-01-04 13:50:05 -0300

    chore: adjust mysql connection settings

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
